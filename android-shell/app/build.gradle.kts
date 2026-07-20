28609fca85f5d420404a06aafae61c4f1c99684b
张航宇 <zhanghangyu@supcon.com>
2026-07-20 13:15:48 +0800

    修改yml

--- android-shell/app/build.gradle.kts
+++ android-shell/app/build.gradle.kts
@@ -13,6 +13,13 @@ android {
     buildTypes {
         release { isMinifyEnabled = false; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro") }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
     buildFeatures { buildConfig = true }
 }
 
